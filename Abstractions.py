86a6e8e3f798d621244ec453a46f39d046b87653
Erick .oG Gutierrez <113930446+ErickG1234@users.noreply.github.com>
2023-01-23 23:03:04 +0000

    code done but errors

--- Abstractions.py
+++ Abstractions.py
@@ -1,41 +1,32 @@
 import random 
-
+play_again = input("Play again? (yes/no): ").lower()
 choices = ["rock","paper", "scissors"]
-
 computer = random.choice(choices)
+user = input("rock, paper, scissors?: ").lower()
 player = None
- 
-while True:
-  choices = ["rock", "paper", "scissors"]
-  while player not in choices: 
-    player = input("rock, paper, scissors?: ").lower() 
-  if player == computer:
-    print("computer: ", computer)
-    print("player: ", player)
-    print("Tie!")
-  elif player == "rock":
-    if computer == "paper":
-      print("computer: ", computer)
-      print("player: ", player)
-      print("You win!")
-  elif player == "scissors":
-    print("computer: ", computer)
-    print("player: ", player)
-    print("You lose!")
-    if computer == "paper":
+
+def play_game(user, computer, choices, play_again):
+  while True:
+    while user not in choices: 
+      if user == computer:
         print("computer: ", computer)
-        print("player: ", player)
-        print("You win!")
+        print("player: ", user)
+        print("Tie!")
+      elif user == "rock":
+        if computer == "paper":
+          print("computer: ", computer)
+          print("player: ", user)
+          print("You win!")
+      elif user == "scissors":
+        print("computer: ", computer)
+        print("player: ", user)
+        print("You lose!")
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", user)
+            print("You win!")
+  while play_again != "yes":
+    print("Bye!")
+    break
+               
 
- 
-  
-  play_again = input("Play again? (yes/no): ").lower()
-
-  if play_again != "yes":
-    break 
-print("Bye!")
-if play_again == "yes":
-    
-    
-          
-    
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from Abstractions import 
+from Abstractions import play_game
+import random
+
+play_again = input("Play again? (yes/no): ").lower()
+choices = ["rock","paper", "scissors"]
+
+computer = random.choice(choices)
+user = input("rock, paper, scissors?: ").lower()
+
+game = play_game(user, computer, choices, play_again)
+print(game)
